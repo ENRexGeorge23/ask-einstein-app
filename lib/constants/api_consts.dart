9a51d72c4ca6b4d579d9230044fa2a53cea25ee5
ENRexGeorge23 <darichonne@gmail.com>
2023-07-15 09:07:52 +0800

    [FIXES]Updates

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "";
+String API_KEY = "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";
 // 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
 // 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
